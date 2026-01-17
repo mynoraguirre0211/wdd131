b037a14db14f7d9e5c65a6f20709aa6c8cd68006
mynoraguirre0211 <mynor.aguirre.0211@gmail.com>
2026-01-17 16:26:27 -0600

    last commit

--- scripts/temples.js
+++ scripts/temples.js
@@ -1,11 +1,9 @@
 const hamButton = document.querySelector("#menu");
 const navigation = document.querySelector(".navigation");
-const navselector = document.querySelectorAll("header");
 
 hamButton.addEventListener("click", () => {
     navigation.classList.toggle("open");
     hamButton.classList.toggle("open");
-    navselector.classList.toggle("open");
 });
 
 const currentYearSpan = document.getElementById('currentyear');
