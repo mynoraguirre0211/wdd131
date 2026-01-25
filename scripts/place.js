ed357c3565a29c744846feeac13cf71ea4d78880
mynoraguirre0211 <mynor.aguirre.0211@gmail.com>
2026-01-24 23:30:09 -0600

    1st creation

---
+++ scripts/place.js
@@ -0,0 +1,7 @@
+const currentYearSpan = document.getElementById('currentyear');
+const lastModified = document.getElementById('lastModified');
+
+const currentYear = new Date().getFullYear();
+
+currentYearSpan.textContent = currentYear;
+lastModified.textContent = `Last Modified: ${document.lastModified}`; 
