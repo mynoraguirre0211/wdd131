a74412c6b9aad356d095edc67f93f5e7ef1a5b4e
mynoraguirre0211 <mynor.aguirre.0211@gmail.com>
2026-01-26 14:57:17 -0600

    almost done

--- scripts/place.js
+++ scripts/place.js
@@ -4,4 +4,26 @@ const lastModified = document.getElementById('lastModified');
 const currentYear = new Date().getFullYear();
 
 currentYearSpan.textContent = currentYear;
-lastModified.textContent = `Last Modified: ${document.lastModified}`; 
+lastModified.textContent = `Last Modified: ${document.lastModified}`;
+
+const temperature = 10; // °C
+const windSpeed = 5; // km/h
+
+document.getElementById("temp").textContent = temperature;
+document.getElementById("wind").textContent = windSpeed;
+
+function calculateWindChill(temp, wind) {
+    return (
+        13.12 +
+        0.6215 * temp -
+        11.37 * Math.pow(wind, 0.16) +
+        0.3965 * temp * Math.pow(wind, 0.16)
+    ).toFixed(1);
+}
+
+let windChill = "N/A";
+if (temperature <= 10 && windSpeed > 4.8) {
+    windChill = `${calculateWindChill(temperature, windSpeed)} °C`;
+}
+
+document.getElementById("windchill").textContent = windChill;
