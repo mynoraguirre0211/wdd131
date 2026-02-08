ee895814803c456ea4ad1390d086453eaf779745
mynoraguirre0211 <mynor.aguirre.0211@gmail.com>
2026-02-07 21:17:27 -0600

    finish

--- scripts/form.js
+++ scripts/form.js
@@ -37,3 +37,13 @@ const products = [
         averagerating: 5.0
     }
 ];
+
+const select = document.querySelector("#product");
+
+products.forEach(product => {
+    const option = document.createElement("option");
+    option.value = product.id;
+    option.textContent = product.name;
+    select.appendChild(option);
+});
+
